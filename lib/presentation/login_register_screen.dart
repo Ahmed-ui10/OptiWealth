b0b498cbbcb78b169e90a129856edb8a0530ba0c
AhmedSayed_10 <20240030@stud.fci-cu.edu.eg>
2026-04-24 17:40:33 +0300

    update main

---
+++ lib/presentation/login_register_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class LoginRegisterScreen extends StatelessWidget {
+  const LoginRegisterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+       
+        title:  Center(
+          child: Text('OptiWealth',)),
+      ),
+      body: const Center(
+        child: Text('Welcome to the Login/Register Screen'),
+      ),
+    );
+  }
+}

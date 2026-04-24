88e09fbd1669ae1becac690b3b496911244c24da
AhmedSayed_10 <20240030@stud.fci-cu.edu.eg>
2026-04-24 17:50:23 +0300

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -15,8 +15,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'OptiWealth',
-
       home: const LoginRegisterScreen(),
     );
   }

2e0ea734115f3f79aca2ab05b5947f190c691c05
I-Bakr <127116084+I-Bakr@users.noreply.github.com>
2026-05-02 14:48:53 +0300

    Refactor addTransaction and include category name
    Refactor addTransaction method for improved readability and add category name to budget tracking.

--- lib/services/transaction_service.dart
+++ lib/services/transaction_service.dart
@@ -2,27 +2,45 @@ import '../models/transaction_model.dart';
 import '../repositories/transaction_repository.dart';
 import '../repositories/budget_repository.dart';
 import '../repositories/account_repository.dart';
+import '../repositories/category_repository.dart';
 import 'budget_service.dart';
 
 class TransactionService {
   final TransactionRepository _transactionRepo = TransactionRepository();
   final AccountRepository _accountRepo = AccountRepository();
+  final CategoryRepository _categoryRepo = CategoryRepository();
   final BudgetService _budgetService = BudgetService();
 
   Future<void> addTransaction(Transaction transaction) async {
     if (!transaction.validate()) throw Exception('Invalid transaction');
+    
     await _transactionRepo.createTransaction(transaction);
+    
     final account = await _accountRepo.getAccountByUserId(transaction.userId);
-    if (account != null) {
-      if (transaction.transactionType) {
+    if (account != null)
+    {
+      if (transaction.transactionType)
+      {
         account.deposit(transaction.amount);
-      } else {
+      }
+      else
+      {
         account.withdraw(transaction.amount);
       }
       await _accountRepo.updateAccount(account);
     }
-    if (!transaction.transactionType) {
-      await _budgetService.updateBudgetTracking(transaction.userId, transaction.categoryId, transaction.amount);
+
+    if (!transaction.transactionType)
+    {
+      final category = await _categoryRepo.getCategoryById(transaction.categoryId);
+      final String categoryName = category?.name ?? "Unknown Category";
+
+      await _budgetService.updateBudgetTracking(
+        transaction.userId, 
+        transaction.categoryId, 
+        transaction.amount,
+        categoryName,
+      );
     }
   }
 
